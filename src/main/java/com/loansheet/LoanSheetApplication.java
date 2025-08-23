4baa95250e30718cd2d694523d4817a0d4c453a2
YonatanQuintero <yonatan.a.quintero.r@gmail.com>
2025-08-23 13:53:46 -0500

    This is where it all begins...

---
+++ src/main/java/com/loansheet/LoanSheetApplication.java
@@ -0,0 +1,13 @@
+package com.loansheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanSheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loansheet/LoanSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loansheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
